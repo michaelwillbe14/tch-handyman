464892645bfc8e1ae6a85457c190a7746fe7008b
Michael Willoughy <youremail@example.com>
2025-12-18 16:10:08 -0700

    app/layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,15 @@
-/* eslint-disable no-unused-vars */
+// app/layout.jsx
+import "./globals.css";
+
 export const metadata = {
-  title: "Tooele Handyman Services",
-  description:
-    "Local • Licensed • Insured handyman services in Tooele and surrounding areas.",
+  title: "TC Handyman",
+  description: "Handyman services in Tooele County",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
